3d8081e16b19f8c3b184a3ae2a69fc7aad158d97
andy2148deathedge <andy2148deathedge@gmail.com>
2022-04-13 23:35:20 +0800

    leetcode 35 by binary search

--- 刷題leet code/AC建議起手10題/7. Search Insert Position.js
+++ 刷題leet code/AC建議起手10題/7. Search Insert Position.js
@@ -11,12 +11,45 @@ You must write an algorithm with O(log n) runtime complexity.
  * param {number} target
  * return {number}
  */
- var searchInsert = function(nums, target) {
+
+/*
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ */
+
+var searchInsert = function(nums, target) {
+  let left = 0,
+      right = nums.length - 1;
+  let mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
     
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target < nums[mid]) { 
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    } 
+  }
+  
+  // 目標不在 test case array 的情況, 最後 right + 1 會等於 left 
+  return left;
 };
 
 
 // main
-const numsArray = [0,0,1,1,1,2,2,3,3,4];
-const result = removeDuplicates(numsArray);
+const nums = [1,3,5,6];
+const target = 7;
+const result = searchInsert(nums, target);
 console.log(result);
